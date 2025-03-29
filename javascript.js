19a13f6b11c94385d606c9644f9f46b934fbdffe
rainepj <rainepj20@gmail.com>
2025-03-28 21:33:38 -0400

    Added UI for Rock Paper Scissors. - Added:     - Buttons for human choices & reset     - Scoreboard     - Log of wins/losses UI required new lengthy CSS file

--- javascript.js
+++ javascript.js
@@ -1,15 +1,6 @@
 let computerScore = 0;
 let humanScore = 0;
 
-function whoWon(computerScore, humanScore){
-    let endScore = Math.abs(computerScore-humanScore);
-    if (computerScore > humanScore){
-        console.log(`The computer beat you by ${endScore}!`)
-    } else if (humanScore > computerScore){
-        console.log(`Human beat computer. Human won by ${endScore}!`)
-    }
-}
-
 function getComputerChoice(){
     let x = Math.random()
     if (x < 0.32){
@@ -21,62 +12,123 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt("Rock, Paper, or Scissors? What's your choice?")
-    if (choice.toLowerCase() === "rock"){
-        return "rock";
-    } else if (choice.toLowerCase() === "paper"){
-        return "paper";
-    } else if (choice.toLowerCase() === "scissors"){
-        return "scissors";
-    } else {
-        console.log("please choose from the three options.")
-    }
+function rockChoice(){
+    let humanChoice = "rock";
+    playGame(humanChoice);
+}
+function scissorChoice(){
+    let humanChoice = "scissors";
+    playGame(humanChoice);
+}
+function paperChoice(){
+    let humanChoice = "paper";
+    playGame(humanChoice);
+}
+
+function roundResult(roundText){
+    const li = document.createElement("li");
+    li.textContent = roundText;
+    ol.appendChild(li);
 }
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice == "rock" && computerChoice == "rock"){
-        console.log("It's a tie!")
+        let roundText = "It's a tie";
+        roundResult(roundText);
     } else if (humanChoice == "rock" && computerChoice == "scissors"){
-        console.log("You Win! Rock beats Scissors.")
+        let roundText = "You Win! Rock beats Scissors!";
+        roundResult(roundText);
         humanScore++;
     } else if (humanChoice == "rock" && computerChoice == "paper"){
-        console.log("You Lose! Paper beats Rock")
+        let roundText = "You Lose! Paper beats Rock.";
+        roundResult(roundText);
         computerScore++;
     } else if (humanChoice == "paper" && computerChoice == "rock"){
-        console.log("You Win! Paper beats Rock!")
+        let roundText = "You Win! Paper beats Rock!";
+        roundResult(roundText);
         humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "paper"){
-        console.log("It's a tie!")
+        let roundText = "It's a tie";
+        roundResult(roundText);
     } else if (humanChoice == "paper" && computerChoice == "scissors"){
-        console.log("You Lose! Scissors beats Paper")
+        let roundText = "You Lose! Scissors beats Paper.";
+        roundResult(roundText);
         computerScore++;
     } else if (humanChoice == "scissors" && computerChoice == "rock"){
-        console.log("You Lose! Rock beats Scissors")
+        let roundText = "You Lose! Rock beats Scissors.";
+        roundResult(roundText);
         computerScore++;
     } else if (humanChoice == "scissors" && computerChoice == "paper"){
-        console.log("You Win! Scissors beats paper")
+        let roundText = "You Win! Scissors beats paper!";
+        roundResult(roundText)
         humanScore++;
     } else if (humanChoice == "scissors" && computerChoice == "scissors"){
-        console.log("It's a tie!")
+        let roundText = "It's a tie";
+        roundResult(roundText);
     }
+    compScore.textContent = computerScore;
+    yourScore.textContent = humanScore;
 }
 
-function playGame(){
-    let humanChoice = getHumanChoice();
+function playGame(humanChoice){
     let computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
 }
 
-function playGame5(){
-    let game = 0;
-    while (game < 5){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        game++;
+function whoWon(computerScore, humanScore){
+    let endScore = Math.abs(computerScore-humanScore);
+    if (computerScore > humanScore){
+        console.log(`The computer beat you by ${endScore}!`)
+    } else if (humanScore > computerScore){
+        console.log(`Human beat computer. Human won by ${endScore}!`)
     }
-    whoWon(computerScore, humanScore);
+}
+function hideReveal (){
+    if (ol.getAttribute("class") === "hide"){
+        ol.setAttribute("class", "reveal");
+    } else if (ol.getAttribute("class") === "reveal"){
+        ol.setAttribute("class", "hide");
+    }    
 }
 
+function reset (){
+    const x = document.querySelectorAll("li");
+    let y = Array.from(x);
+    
+    for (let i = 0; i < y.length; i++ ){
+        let tempLi = document.querySelector("li");
+        tempLi.remove();
+    }
+    computerScore = 0;
+    humanScore = 0;
+    compScore.textContent = computerScore;
+    yourScore.textContent = humanScore;
+}
 
+//Interactable
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const reveal = document.querySelector(".reveal");
+const ol = document.querySelector("ol");
+const resetButton = document.querySelector(".reset");
+//Non-interactable
+const yourScore = document.querySelector(".humanScore");
+const compScore = document.querySelector(".computerScore");
+//Interactable
+rock.addEventListener("click",rockChoice);
+paper.addEventListener("click",paperChoice);
+scissor.addEventListener("click",scissorChoice);
+reveal.addEventListener("click",hideReveal);
+resetButton.addEventListener("click", reset);
+
+
+/*
+On rock/paper/scissors button press
+    Computer choice executes -> comparison for
+    win/loss computed -> text containting computer-
+    choice + win/loss should be added as new <li>
+
+
+
+*/
